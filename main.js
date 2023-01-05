7a0cdc06b3885fa57633751ec0b0b6bf11ca75df
angelr1076 <angelr1076@gmail.com>
2023-01-05 15:00:00 -0500

    Added images, containers, CSS properties and JavaScript

---
+++ main.js
@@ -0,0 +1,44 @@
+// slides array
+let slides = document.getElementsByClassName('slider__slide');
+// breadcrumbs array
+let navlinks = document.getElementsByClassName('slider__navlink');
+
+let currentSlide = 0;
+
+function changeSlide(moveTo) {
+  if (moveTo >= slides.length) {
+    moveTo = 0;
+  }
+  if (moveTo < 0) {
+    moveTo = slides.length - 1;
+  }
+
+  slides[currentSlide].classList.toggle('active');
+  navlinks[currentSlide].classList.toggle('active');
+  slides[moveTo].classList.toggle('active');
+  navlinks[moveTo].classList.toggle('active');
+
+  currentSlide = moveTo;
+}
+
+let navNext = document
+  .getElementById('nav-button--next')
+  .addEventListener('click', () => {
+    changeSlide(currentSlide + 1);
+  });
+
+let navPrev = document
+  .getElementById('nav-button--prev')
+  .addEventListener('click', () => {
+    changeSlide(currentSlide - 1);
+  });
+
+let bulletScroll = document
+  .querySelectorAll('.slider__navlink')
+  .forEach((bullet, bulletIndex) => {
+    bullet.addEventListener('click', () => {
+      if (currentSlide !== bulletIndex) {
+        changeSlide(bulletIndex);
+      }
+    });
+  });
